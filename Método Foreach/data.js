0a66972f4367c40384577f9f33625087125dfa11
RICARDO FERNANDES DE MOURA <05380295010@senacrs.edu.br>
2025-04-09 22:00:35 -0300

    .

---
+++ Método Foreach/data.js
@@ -0,0 +1,20 @@
+// export const produtos = [
+//     {
+//         id:1,
+//         name:"TV",
+//         price:2500,
+//         descricao:"Televisão bonita demais"
+//     },
+//     {
+//         id:2,
+//         name:"Geladeira ",
+//         price:4129,
+//         descricao:"Geladeira grande e branca"
+//     },
+//     {
+//         id:3,
+//         name:"Fritadeira sem óleo ",
+//         price:599,
+//         descricao:"Fritadeira das mais boas"
+//     }
+// ]
