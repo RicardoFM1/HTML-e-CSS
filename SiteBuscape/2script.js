ef2d650641a1c6f27ca858ca43181bd2e9f4ebd4
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2025-04-13 03:10:00 -0300

    atualizações site

--- SiteBuscape/2script.js
+++ SiteBuscape/2script.js
@@ -3,13 +3,12 @@ import {data} from "./data.js"
 function carrinhoAbrir(){
     const button = document.querySelector(".carrinhobtn")
     button.addEventListener("click", ()=>{
-        console.log("click")
+        
     const ulcart = document.querySelector(".cartul") 
     if(ulcart){
         ulcart.remove()
     }else{
         const header = document.querySelector("header")
-        console.log(header)
         header.insertAdjacentHTML("beforeend", `
             <ul class="cartul">
                 <li></li>
