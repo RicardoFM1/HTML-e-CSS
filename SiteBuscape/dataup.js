7df7390c5793f76545c91d5cba120e2dbf1d7d6f
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2025-04-19 23:00:41 -0300

    site finalizado

--- SiteBuscape/dataup.js
+++ SiteBuscape/dataup.js
@@ -6,10 +6,10 @@ export const data = {
         name: "Smartphone Apple iPhone 7 128GB",
         images: [
           
-          "./IMAGENS/ImagensCelular/image2.png",
-          "./IMAGENS/ImagensCelular/image3.png",
-          "./IMAGENS/ImagensCelular/image4.png",
-          "./IMAGENS/ImagensCelular/imagemgrande.png",
+          "/IMAGENS/ImagensCelular/image2.png",
+          "/IMAGENS/ImagensCelular/image3.png",
+          "/IMAGENS/ImagensCelular/image4.png",
+          "/IMAGENS/ImagensCelular/imagemgrande.png",
         ],
 
         price: {
@@ -24,10 +24,10 @@ export const data = {
         id: 9971,
         name: "Smart TV Samsung Série 4 UN32J4300AG 50 polegadas LED Plana",
         images: [
-          "./IMAGENS/ImagensTV/imagetv2.png",
-          "./IMAGENS/ImagensTV/imagetv3.png",
-          "./IMAGENS/ImagensTV/imagetv4.png",
-          "./IMAGENS/ImagensTV/imageTvgrande.png",
+          "/IMAGENS/ImagensTV/imagetv2.png",
+          "/IMAGENS/ImagensTV/imagetv3.png",
+          "/IMAGENS/ImagensTV/imagetv4.png",
+          "/IMAGENS/ImagensTV/imageTvgrande.png",
         ],
         price: {
           value: 1139.9,
@@ -41,10 +41,10 @@ export const data = {
         id: 6717131,
         name: "Câmera Digital Canon EOS Rebel T5i 18.0 Megapixels",
         images: [
-          "./IMAGENS/ImagensCamera/imagecamera.png",
-          "./IMAGENS/ImagensCamera/imagecamera2.png",
-          "./IMAGENS/ImagensCamera/imagecamera3.png",
-          "./IMAGENS/ImagensCamera/imagecameragrande.png",
+          "/IMAGENS/ImagensCamera/imagecamera.png",
+          "/IMAGENS/ImagensCamera/imagecamera2.png",
+          "/IMAGENS/ImagensCamera/imagecamera3.png",
+          "/IMAGENS/ImagensCamera/imagecameragrande.png",
         ],
         price: {
           value: 1999.2,
@@ -58,10 +58,10 @@ export const data = {
         id: 6717141,
         name: "Lenovo IdeaPad 310 80UH0004BR Intel Core i7-6500U 2.5 GHz 8192 MB 1024 GB",
         images: [
-          "./IMAGENS/ImagensNotebook/imagenotebook.png",
-          "./IMAGENS/ImagensNotebook/imagenotebook2.png",
-          "./IMAGENS/ImagensNotebook/imagenotebook3.png",
-          "./IMAGENS/ImagensNotebook/imagenotebookgrande.png",
+          "/IMAGENS/ImagensNotebook/imagenotebook.png",
+          "/IMAGENS/ImagensNotebook/imagenotebook2.png",
+          "/IMAGENS/ImagensNotebook/imagenotebook3.png",
+          "/IMAGENS/ImagensNotebook/imagenotebookgrande.png",
         ],
         price: {
           value: 2599.0,
